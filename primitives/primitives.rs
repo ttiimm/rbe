d7f72a463c0803a1ff7843a8aa89ac0e1ffa5c0d
Tim Likarish <likarish@gmail.com>
2019-04-30 22:10:06 -0400

    primitives - examples of primitives and collections

---
+++ primitives/primitives.rs
@@ -0,0 +1,27 @@
+// primitives.rs - examples of primitive data types
+
+fn main() {
+  // Variables can be type annotated
+  let logical: bool = true;
+
+  let a_float: f64 = 1.0; // Regular annotation
+  let an_integer = 5i32; // suffix annotation
+
+  // Or a default will be used
+  let default_float = 3.0; // `f64`
+  let default_integer = 7; // `i32`
+
+  // A type can also be inferred from context
+  let mut inferred_type = 12; // Type i64 is inferred from another line
+  inferred_type = 4294967296i64;
+
+  // A mutable variables value can be changed
+  let mut mutable = 12; // Mutable `i32`
+  mutable = 21;
+
+  // Error! The type of a variable can't be changed
+  mutable = true;
+
+  // Variables can be overwritten with shadowing
+  let mutable = true;
+}
